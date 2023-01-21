297d53a07397814c7b39d14107a2d170007eda4a
Shlomo666 <shlomo366@gmail.com>
2023-01-21 20:15:02 +0200

    Remove support of bookmarklets

--- src/actions/invokeBookmark.ts
+++ src/actions/invokeBookmark.ts
@@ -1,9 +1,6 @@
 export function invokeBookmark(url: string = '') {
   // eslint-disable-next-line no-script-url
-  if (url.startsWith('javascript:')) {
-    chrome.tabs.executeScript({ code: url.slice(11) });
-    window.close();
-  } else {
+  if (!url.startsWith('javascript:')) {
     chrome.tabs.create({ url });
   }
 }
